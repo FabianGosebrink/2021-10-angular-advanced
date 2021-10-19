9b196dc27a5cc594b90ef19e95f6660f61b09366
FabianGosebrink <fabian.gosebrink@outlook.com>
2021-10-19 10:23:02 +0200

    Added first tests

--- src/app/todo/container/todo/todo.component.spec.ts
+++ src/app/todo/container/todo/todo.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { Todo } from '../../services/todo';
 import { TodoService } from '../../services/todo.service';
 import { TodoServiceMock } from '../../services/todo.service-mock';
 import { TodoComponent } from './todo.component';
@@ -6,6 +8,7 @@ import { TodoComponent } from './todo.component';
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let service: TodoService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,10 +25,53 @@ describe('TodoComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    service = TestBed.inject(TodoService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it(
+      'should call correct method when loading',
+      waitForAsync(() => {
+        const spy = spyOn(service, 'getAllTodos').and.returnValue(of([]));
+
+        fixture.detectChanges(); // calls ngOnInit()
+
+        fixture.whenStable().then(() => {
+          expect(component.items).toEqual([]);
+          expect(spy).toHaveBeenCalledTimes(1);
+        });
+      })
+    );
+  });
+
+  describe('markAsDone', () => {
+    it(
+      'should call correct method with correct done boolean',
+      waitForAsync(() => {
+        const spy = spyOn(service, 'updateTodo').and.returnValue(of(null));
+
+        const todo = {
+          id: '5',
+          created: 'someDate',
+          value: 'wash car',
+          done: false,
+        } as Todo;
+
+        component.markAsDone(todo);
+
+        fixture.whenStable().then(() => {
+          expect(spy).toHaveBeenCalledOnceWith({
+            id: '5',
+            created: 'someDate',
+            value: 'wash car',
+            done: true,
+          });
+        });
+      })
+    );
   });
 });
